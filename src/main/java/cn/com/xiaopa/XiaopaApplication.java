881bad1140901c5e4aac52e71268dee5bf0ea5c2
wangshuan <sum02468@163.com>
2023-04-03 09:32:47 +0800

    项目初始化

---
+++ src/main/java/cn/com/xiaopa/XiaopaApplication.java
@@ -0,0 +1,13 @@
+package cn.com.xiaopa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaopaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaopaApplication.class, args);
+    }
+
+}
